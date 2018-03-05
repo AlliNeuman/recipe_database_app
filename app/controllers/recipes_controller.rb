49e6316d3405cfb85dbf68b7b32ae0e3eb24ab56
Allison Tzadok <allisontzadok@gmail.com>
2018-03-05 22:09:38 +0000

    addedall get and post, patch and delete routes for recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,43 @@
 class RecipesController < ApplicationController
 
+  get '/recipes/new' do
+    erb :'recipes/new'
+  end
 
-end 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :'recipes/recipes'
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :'recipes/show'
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :'recipes/edit'
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.directions = params[:directions]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+
+end
