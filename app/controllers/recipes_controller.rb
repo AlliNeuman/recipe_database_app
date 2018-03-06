7a2abe54930a87ebdc74c925ee4197bbc8e6cbf6
Allison Tzadok <allisontzadok@gmail.com>
2018-03-06 23:16:59 +0000

    added a partial form to the recipes/new page and made sure connection worked

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,18 @@
 class RecipesController < ApplicationController
 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :'recipes/index'
+  end
+
   get '/recipes/new' do
     erb :'recipes/new'
   end
 
-  get '/recipes' do
-    @recipes = Recipe.all
-    erb :'recipes/index'
+  post '/recipes' do
+    raise params.inspect
+    # @recipe = Recipe.create(params)
+    # redirect to "/recipes/#{@recipe.id}"
   end
 
   get '/recipes/:id' do
@@ -24,13 +30,8 @@ class RecipesController < ApplicationController
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
-    @recipe.directions = params[:directions]
+    recipe.directions = params[:directions]
     @recipe.save
-    redirect to "/recipes/#{@recipe.id}"
-  end
-
-  post '/recipes' do
-    @recipe = Recipe.create(params)
     redirect to "/recipes/#{@recipe.id}"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-Colin = User.create(:username => "colinthejeepman", :email => "colin@colin.com" :password => "colinpw")
+@colin = User.create(:username => "colinthejeepman", :email => "colin@colin.com", :password => "colinpw")
+@alli = User.create(:username => "allithedev", :email => "alli@alli.com", :password => "allipw")
+@dave = User.create(:username => "davethegolfer", :email => "dave@dave.com", :password => "davepw")
+@stacy = User.create(:username => "stacythebaker", :email => "stacy@stacy.com", :password => "stacypw", )
 
-Alli = User.create(:username => "allithedev", :email => "alli@alli.com" :password => "allipw")
+buffalo_chicken_dip = Recipe.new(:name =>"Buffalo Chicken Dip", :user_id => @stacy.id)
+buffalo_chicken_dip.cook_time = "30 minutes"
+buffalo_chicken_dip_ingredients = ["chicken breasts", "cream cheese", "ranch dressing", "buffalo sauce", "shredded cheddar"]
+buffalo_chicken_dip_ingredients.each {|i| buffalo_chicken_dip.ingredients.build(name: i)}
+buffalo_chicken_dip.directions = ["Mix all ingredients","Place in greased 2 quart dish", "Bake 350 degrees", "Serve with Chips"]
+buffalo_chicken_dip.save
 
-Dave = User.create(:username => "davethegolfer", :email => "dave@dave.com" :password => "davepw")
+moms_meatloaf = Recipe.new(:name => "Mom's Meatloaf", :user_id => @stacy.id)
+moms_meatloaf.cook_time = "60 minutes"
+moms_meatloaf_ingredients = ["ground beef", "egg", "breadcrumbs", "onion", "milk", "Heinz 57 hickory smoke", "salt", "pepper"]
+moms_meatloaf_ingredients.each {|i| moms_meatloaf.ingredients.build(name: i)}
+moms_meatloaf.directions = ["Mix all ingredients", "Shape in a loaf pan", "Bake 350 degrees"]
+moms_meatloaf.save
+# blueberry_pancakes = Recipe.new(name: 'Blueberry Pancakes')
+# blueberry_pancakes_ingredients = ['All-purpose flour', 'Sugar', 'Baking powder', 'Baking soda', 'Salt', 'Large eggs', 'Milk', 'Sour cream', 'Butter', 'Vanilla extract', 'Blueberries', 'Lemon zest']
+# blueberry_pancakes_ingredients.each{|i| blueberry_pancakes.ingredients.build(name: i)}
+# blueberry_pancakes.save
 
-Stacy = User.create(:username => "stacythebaker", :email => "stacy@stacy.com" :password => "stacypw", )
+# bacon_pancakes = Recipe.new(name: 'Bacon Pancakes')
+# bacon_pancakes_ingredients = ['Some bacon', 'Pancake']
+# bacon_pancakes_ingredients.each{|i| bacon_pancakes.ingredients.build(name: i)}
+# bacon_pancakes.save
