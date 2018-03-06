538e1a9a1214abcf5d5db68a52cace5951e1ef43
Allison Tzadok <allisontzadok@gmail.com>
2018-03-06 20:32:45 +0000

    added logged_in verification and redirects to the ingredients_controller page

--- app/controllers/ingredients_controllers.rb
+++ app/controllers/ingredients_controllers.rb
@@ -1,8 +1,60 @@
 class IngredientsController < ApplicationController
 
-  get 'ingredients/:id/' do
-    @ingredient = Ingredient.find(params[:id])
-    erb :'ingredients/show'
+  get '/ingredients' do
+    if logged_in?
+      @ingredients = Ingredient.all
+
+      erb :'ingredients/index'
+    else
+      redirect to '/login'
+    end
   end
+
+  get '/ingredients/new' do
+    if logged_in?
+      erb :'ingredients/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/ingredients/:id' do
+    if logged_in?
+      @ingredient = Ingredient.find(params[:id])
+      erb :'ingredients/show'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/ingredients/:id/edit' do
+    if logged_in?
+      @ingredient = Ingredient.find(params[:id])
+      if @ingredient && @ingredient.user == current_user
+        erb :'ingredients/edit'
+      else
+        redirect to "/ingredients/#{@ingredient.id}"
+      end
+    else
+      redirect to '/login'
+    end
+  end
+  post '/ingredients' do
+
+
+  end
+
+  patch '/recipe/:id' do
+
+  end
+
+  delete '/recipe/:id' do
+
+  end
+
+end
+
+
+
 
 end
